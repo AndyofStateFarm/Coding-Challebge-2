cc9c09427ccd066c00de5e251b1f4726dbadae7c
AndyofStateFarm <andrewjb97@yahoo.com>
2024-05-16 15:49:38 -0400

    Add output functionality to display bill, tip, and total.

--- cc_1.js
+++ cc_1.js
@@ -1,3 +1,8 @@
 // Inital setup up of a ternary operator to calculate tip
-var bill = 20;
-var tip = (bill <= 50 || bill >= 300) ? bill*.15 : bill*.20;
+let bill = 20;
+let tip = (bill <= 50 || bill >= 300) ? bill*.15 : bill*.20;
+let total = tip + bill;
+
+
+// Output to console
+console.log("The bill was $" + bill+ ", tip was $" + tip + ", the total value $" + total);
