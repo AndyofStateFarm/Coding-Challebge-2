00720d6c4ea778c6609821bd2821341f87d2900f
AndyofStateFarm <andrewjb97@yahoo.com>
2024-05-16 17:47:44 -0400

    Create and test calcTip function.

--- cc_1.js
+++ cc_1.js
@@ -1,8 +1,13 @@
-// Inital setup up of a ternary operator to calculate tip
-let bill = 20;
-let tip = (bill <= 50 || bill >= 300) ? bill*.15 : bill*.20;
-let total = tip + bill;
+// Function that inputs any bill and returns the tip
+let bill = 100;
+function calcTip(bill) {
+    // Inital setup up of a ternary operator to calculate tip
+    let tip = (bill <= 50 || bill >= 300) ? bill*.15 : bill*.20;
+    return tip
+}
 
+let TIP = calcTip(bill)
+let total = TIP + bill;
 
 // Output to console
-console.log("The bill was $" + bill+ ", tip was $" + tip + ", the total value $" + total);
+console.log("The bill was $" + bill + ", tip was $" + TIP + ", the total value $" + total);
