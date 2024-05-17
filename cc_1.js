3e1a1e2537ee7c8b6786b697d8bb4af3bfc80e00
AndyofStateFarm <andrewjb97@yahoo.com>
2024-05-16 20:12:48 -0400

    Add output functionality to display bill, tip, and total.

--- cc_1.js
+++ cc_1.js
@@ -1,13 +1,30 @@
+//U08282838
+  
 // Function that inputs any bill and returns the tip
-let bill = 100;
 function calcTip(bill) {
     // Inital setup up of a ternary operator to calculate tip
-    let tip = (bill <= 50 || bill >= 300) ? bill*.15 : bill*.20;
-    return tip
+   let tip = (bill <= 50 || bill >= 300) ? bill*.15 : bill*.20;
+      return tip
 }
 
-let TIP = calcTip(bill)
-let total = TIP + bill;
+// set arrays
+let bills = [275,40,430,125,555,44];
+let tips = []
+let totals = []
 
-// Output to console
-console.log("The bill was $" + bill + ", tip was $" + TIP + ", the total value $" + total);
+// loop test bills to generate tips and totals
+for (const bill of bills) {
+  
+    let Tip = calcTip(bill)
+    let total = Tip + bill;
+
+tips.push(Tip)
+totals.push(total)
+
+    // Output to console
+    console.log("The bill was $" + bill + ", tip was $" + Tip + ", the total value $" + total);
+
+}
+
+// Output arrays to console
+console.log(bills, tips, totals)
